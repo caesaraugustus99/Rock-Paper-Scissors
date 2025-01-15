6ed7c4b7afb45a6220389d9b8046a10076e13992
August Bruno <176425695+caesaraugustus99@users.noreply.github.com>
2025-01-14 23:56:22 -0800

    Add random number generator logic for computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log('Hello World!')
+
+// Pseudo Code
+// --
+// Prompt 1: Your game will be played against the computer. You will write a function that randomly returns “rock”, “paper” or “scissors”.
+// 
+// Create function for computer's choice.
+// Generate an integer between 1 and 3.
+// IF the integer is equal to 1 THEN
+    // choose rock.
+// ELSE IF the integer is equal to 2 THEN
+    // choose paper.
+// ELSE if the integer is equal to 3 THEN
+    // choose scissors.
+
+function getComputerChoice() {
+    if (randomChoice === 1) {
+        console.log('Rock!');
+    } else if (randomChoice === 2) {
+        console.log('Paper!');
+    } else {
+        console.log('Scissors!');;
+    }
+}
+
+let randomChoice = Math.floor(Math.random() * 3) + 1;
+
+getComputerChoice();

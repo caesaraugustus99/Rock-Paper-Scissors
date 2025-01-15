c9cfeee70fb5db51b3c58cc44562a07010a3d2e6
August Bruno <176425695+caesaraugustus99@users.noreply.github.com>
2025-01-15 01:12:53 -0800

    Add logic for valid choices for human input

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('Hello World!')
-
 // Pseudo Code
 // --
 // Prompt 1: Your game will be played against the computer. You will write a function that randomly returns “rock”, “paper” or “scissors”.
@@ -10,19 +8,42 @@ console.log('Hello World!')
     // choose rock.
 // ELSE IF the integer is equal to 2 THEN
     // choose paper.
-// ELSE if the integer is equal to 3 THEN
+// ELSE
     // choose scissors.
 
 function getComputerChoice() {
     if (randomChoice === 1) {
-        console.log('Rock!');
+        console.log('Your opponent chose rock!');
     } else if (randomChoice === 2) {
-        console.log('Paper!');
+        console.log('Your opponent chose paper!');
     } else {
-        console.log('Scissors!');;
+        console.log('Your opponent chose scissors!');
     }
 }
 
 let randomChoice = Math.floor(Math.random() * 3) + 1;
 
 getComputerChoice();
+
+//  Pseudo Code
+// --
+// Prompt 2: Your game will be played by a human player. You will write a function that takes the user choice and returns it.
+// 
+// Create function for human's choice.
+// Prompt for human input.
+// IF valid choice is selected THEN
+    // Return valid choice.
+// ELSE
+    // User must try again.
+
+function getHumanChoice() {
+    let inputChoice = prompt('Please choose rock, paper, or scissors.').toLowerCase();
+    if (inputChoice == 'rock' || inputChoice == 'paper' || inputChoice == 'scissors') {
+        console.log(`You chose ${inputChoice}`);
+    } else {
+        console.log('Invalid option, try again.');
+        return getHumanChoice();
+    }
+}
+
+getHumanChoice();

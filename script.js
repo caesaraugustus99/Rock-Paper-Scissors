81e7320389399f5361df269e4e9aa6265ef66b17
August Bruno <176425695+caesaraugustus99@users.noreply.github.com>
2025-01-15 21:37:30 -0800

    Add logic using FOR loop to play 5 rounds of the game Move playRound function inside of new function, playGame.

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function getComputerChoice() {
     }
 }
 
-//  Pseudo Code
+// Pseudo Code
 // --
 // Prompt 2: Your game will be played by a human player. You will write a function that takes the user choice and returns it.
 // 
@@ -48,10 +48,7 @@ function getHumanChoice() {
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-//  Pseudo Code
+// Pseudo Code
 // --
 // Prompt 3: Your game will be played round by round. You will write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
 // 
@@ -64,29 +61,81 @@ let computerScore = 0;
     // Display defeat message.
 // ELSE
     // Display tie message.
+// --
+// 
+// MOVED TO playGame:
+// 
+    // function playRound() {
+    //     let computerChoice = getComputerChoice();
+    //     let humanChoice = getHumanChoice();
+    //     console.log(`computerChoice: ${computerChoice} humanChoice: ${humanChoice}`)
+        
+    //     if (humanChoice == computerChoice) {
+    //         console.log('It\'s a tie!');
+    //     }
+    //     else if ((humanChoice == 'rock' && computerChoice == 'scissors') || (humanChoice == 'paper' && computerChoice == 'rock') || (humanChoice == 'scissors' && computerChoice == 'paper')) {
 
-function playRound() {
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
-    console.log(`computerChoice: ${computerChoice} humanChoice: ${humanChoice}`)
+    //         console.log(`${humanChoice} beats ${computerChoice}!`);
+    //         humanScore++;
+
+    //     } else {
+
+    //         console.log(`${computerChoice} beats ${humanChoice}!`);
+    //         computerScore++;
+
+    //     }
+    // }
+    // 
+    // playRound();
+    // 
+    // console.log(`Your score is ${humanScore}`);
+    // console.log(`The computer's score is ${computerScore}`);
+// 
+// --
+// Pseudo Code
+// --
+// Prompt 4: Your game will play 5 rounds. You will write a function named playGame that calls playRound to play 5 rounds, keeps track of the scores and declares a winner at the end.
+// 
+// Create function for playing multiple rounds of the game.
+// Create variable for the number of rounds.
+// Loop the round function 5 times.
+// Display a message naming the victor.
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let roundNumber = 1;
+
+    function playRound() {
+        console.log(`This is Round ${roundNumber} (out of 5)`)
+
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+        
+        if (humanChoice == computerChoice) {
+            console.log('It\'s a tie!');
+        }
+        else if ((humanChoice == 'rock' && computerChoice == 'scissors') || (humanChoice == 'paper' && computerChoice == 'rock') || (humanChoice == 'scissors' && computerChoice == 'paper')) {
     
-    if (humanChoice == computerChoice) {
-        console.log('It\'s a tie!');
+            console.log(`${humanChoice} beats ${computerChoice}!`);
+            humanScore++;
+            console.log(`Your score is ${humanScore}`);
+            console.log(`The computer's score is ${computerScore}`);
+    
+        } else {
+    
+            console.log(`${computerChoice} beats ${humanChoice}!`);
+            computerScore++;
+            console.log(`Your score is ${humanScore}`);
+            console.log(`The computer's score is ${computerScore}`);
+    
+        }
     }
-    else if ((humanChoice == 'rock' && computerChoice == 'scissors') || (humanChoice == 'paper' && computerChoice == 'rock') || (humanChoice == 'scissors' && computerChoice == 'paper')) {
 
-        console.log(`${humanChoice} beats ${computerChoice}!`);
-        humanScore++;
-
-    } else {
-
-        console.log(`${computerChoice} beats ${humanChoice}!`);
-        computerScore++;
-
-    }
+    for (; roundNumber <= 5; roundNumber++) {
+        playRound();
+        console.log(roundNumber);
+}
 }
 
-playRound();
-
-console.log(`Your score is ${humanScore}`);
-console.log(`The computer's score is ${computerScore}`);
+playGame();
